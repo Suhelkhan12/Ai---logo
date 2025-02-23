891a9e2c40c5498917e259abe4b644a2dd7e1e7a
Suhel <suhel7822@gmail.com>
2025-02-23 22:45:34 +0530

    separate generate logo cta for client interaction

--- app/create-logo/_components/LogoData.tsx
+++ app/create-logo/_components/LogoData.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
 import { LogoDataDesign, ChangeLogoData } from "./logo-data-design";
+import GenerateLogo from "./generate-logo";
 
 const LogoData = () => {
   return (
     <>
       <h1 className="text-4xl md:text-5xl font-extrabold font-sans text-primary">
-        Your Logo Data
+        Data entered for generating logo
       </h1>
       <p className="text-base md:text-lg mt-2">
         Below is the information you provided to generate your logo.
@@ -15,7 +15,7 @@ const LogoData = () => {
 
       <div className="mt-10 flex items-center justify-center gap-10">
         <ChangeLogoData />
-        <Button>Generate logo</Button>
+        <GenerateLogo />
       </div>
     </>
   );
---
+++ app/create-logo/_components/generate-logo.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const GenerateLogo = () => {
+  return <Button>Generate logo</Button>;
+};
+
+export default GenerateLogo;
